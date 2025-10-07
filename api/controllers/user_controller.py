77c3c6638786adc726c5c110794371e425a18c40
Luan Souza <luansouzac14@gmail.com>
2025-10-07 19:09:59 -0300

    feat: Adiciona todas as rotas padrões de usuario

--- api/controllers/user_controller.py
+++ api/controllers/user_controller.py
@@ -2,12 +2,6 @@ from flask import jsonify
 from models.user_model import Users
 from models import db
 
-def create_user(user_data):
-    user = Users(**user_data)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
-
 def get_users():
     users = Users.query.all()
     return jsonify([u.to_dict() for u in users])
@@ -17,6 +11,32 @@ def get_user_by_id(user_id):
     if user:
         return jsonify(user.to_dict())
     return jsonify({"error": "Usuário não encontrado"}), 404
+
+def create_user(user_data):
+    user = Users(**user_data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.to_dict()), 201
+
+def update_user(user_id, user_data):
+    user = Users.query.get(user_id)
+    if not user:
+        return jsonify({"error": "Usuário não encontrado"}), 404
+
+    for key, value in user_data.items():
+        setattr(user, key, value)
+
+    db.session.commit()
+    return jsonify(user.to_dict())
+
+def delete_user(user_id):
+    user = Users.query.get(user_id)
+    if not user:
+        return jsonify({"error": "Usuário não encontrado"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "Usuário deletado com sucesso"}), 200
 
 # def get_user_by_id(user_id):
 #     user = next((u for u in users if u["id"] == user_id), None)
