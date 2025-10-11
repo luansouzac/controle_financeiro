e437bd3af009e6b4d42f5e688e7643430de0429a
Luan Souza <luansouzac14@gmail.com>
2025-10-11 19:52:20 -0300

    feat: Aumenta tempo de expiracao do access token

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from models import db
 from flask_jwt_extended import JWTManager
 import os
+from datetime import timedelta
 
 load_dotenv()
 
@@ -14,6 +15,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config["JWT_SECRET_KEY"] = "123456789"  # Chave secreta demais
 app.config['JSON_SORT_KEYS'] = False
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+
 
 db.init_app(app)
 jwt = JWTManager(app)
