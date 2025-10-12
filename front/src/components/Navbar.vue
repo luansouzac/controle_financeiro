53bd6f6cb88a96a1283c322983309c3a510e85b1
Luan Souza <luansouzac14@gmail.com>
2025-10-12 13:19:20 -0300

    feat: Reorganiza a navbar

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,85 +1,78 @@
 <template>
-  <v-navigation-drawer
-    expand-on-hover
-    permanent
-    rail
-    color="#0A2A4D"
-    theme="dark"
-  >
+  <v-navigation-drawer expand-on-hover permanent rail color="#0A2A4D" theme="dark">
     <v-list>
       <v-list-item
         prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
         subtitle="sandra_a88@gmail.com"
         title="Sandra Adams"
+        to="/profile"
       ></v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home-outline" title="Home" value="home" to="/"></v-list-item>
       <v-list-item
         prepend-icon="mdi-view-dashboard-outline"
-        title="Início"
+        title="Dashboard"
         value="dashboard"
+        to="/dashboard"
       ></v-list-item>
       <v-list-item
         prepend-icon="mdi-swap-horizontal"
         title="Transações"
         value="transactions"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-wallet-outline"
-        title="Carteiras"
-        value="wallets"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-credit-card-outline"
-        title="Cartões"
-        value="cards"
-      ></v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
-    <v-list density="compact" nav>
-      <v-list-item
-        prepend-icon="mdi-arrow-up-bold-circle-outline"
-        title="Receitas"
-        value="income"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-arrow-down-bold-circle-outline"
-        title="Despesas"
-        value="expenses"
+        to="/transactions"
       ></v-list-item>
       <v-list-item
         prepend-icon="mdi-chart-pie-outline"
         title="Relatórios"
         value="reports"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-target"
-        title="Orçamentos"
-        value="budgets"
+        to="/reports"
       ></v-list-item>
     </v-list>
-    
+
     <v-divider></v-divider>
-    
+
     <v-list density="compact" nav>
       <v-list-item
-        prepend-icon="mdi-account-circle-outline"
-        title="Meu Perfil"
-        value="profile"
+        prepend-icon="mdi-wallet-outline"
+        title="Carteiras"
+        value="wallets"
+        to="/wallets"
       ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-arrow-up-bold-circle-outline"
+        title="Receitas"
+        value="income"
+        to="/income"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-arrow-down-bold-circle-outline"
+        title="Despesas"
+        value="expenses"
+        to="/expenses"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
       <v-list-item
         prepend-icon="mdi-cog-outline"
         title="Configurações"
         value="settings"
+        to="/settings"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-information-outline"
+        title="Sobre"
+        value="about"
+        to="/about"
       ></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
